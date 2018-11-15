c44900405a48fb31039e8c318f98ea378a796988
Nabeel on the Lenovo <you@example.com>
2018-11-15 20:54:02 +0000

    Converted to node.js project

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/elements', function(req, res, next) {
+  res.render('elements', { title: 'Express' });
+});
+
+router.get('/generic', function(req, res, next) {
+  res.render('generic', { title: 'Express' });
+});
+
+module.exports = router;
